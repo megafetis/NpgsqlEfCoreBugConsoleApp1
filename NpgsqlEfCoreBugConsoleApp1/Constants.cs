fd2c6a37b295c025b1d66e37d48669355185f1e9
fetisov <me@efetisov.ru>
2024-05-15 14:02:47 +0300

    g

--- NpgsqlEfCoreBugConsoleApp1/Constants.cs
+++ NpgsqlEfCoreBugConsoleApp1/Constants.cs
@@ -8,6 +8,6 @@ namespace NpgsqlEfCoreBugConsoleApp1
 {
     public static class Constants
     {
-        public const string ConnectionString = "Host=zaprosotvet.ru;Port=5432;Database=new_db;Username=postgres;Password=qwerty!123";
+        public const string ConnectionString = "Host=localhost;Port=5432;Database=new_db;Username=postgres;Password=qwerty";
     }
 }
